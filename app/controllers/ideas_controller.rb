556434ac8f100aca4984eb83c5a3c1c680a34943
kBoris <khvanboris@gmail.com>
2019-09-23 12:05:37 -0700

    completed implementing CRUD actions for ideas

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,16 +1,64 @@
 class IdeasController < ApplicationController
+  layout 'admin'
+
   def index
+    @ideas = Idea.all
   end
 
   def show
+    @idea = Idea.find(params[:id])
   end
 
   def new
+    @idea = Idea.new
+  end
+
+  def create
+    @idea = Idea.new(idea_params)
+    if @idea.save 
+    flash[:notice] = "Created succesfully"
+    redirect_to(ideas_path)
+    else 
+    render('new')
+    end
   end
 
   def edit
+    @idea = Idea.find(params[:id])
+  end
+
+  def update
+    @idea = Idea.find(params[:id])
+    if @idea.update_attributes(idea_params)
+      flash[:notice] = "Updated Succesfully!"
+      redirect_to(ideas_path(@idea))
+    else
+      render('edit')
+    end
+
   end
 
   def delete
+    @idea = Idea.find(params[:id])
   end
+
+  def destroy
+    @idea = Idea.find(params[:id])
+    if @idea.destroy 
+      flash[:notice] = "'#{@idea.title}'Destroyed  Succesfully"
+      redirect_to(ideas_path)
+    else
+      render('delete')
+    end
+  end
+
+
+  private
+
+  def idea_params
+    params.require(:idea).permit(:title, :body, :user_id)
+  end
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,18 @@
 Rails.application.routes.draw do
-  get 'members/index'
-  get 'members/show'
-  get 'members/new'
-  get 'members/edit'
-  get 'members/delete'
-  get 'ideas/index'
-  get 'ideas/show'
-  get 'ideas/new'
-  get 'ideas/edit'
-  get 'ideas/delete'
-  get 'users/index'
-  get 'users/show'
-  get 'users/new'
-  get 'users/edit'
-  get 'users/delete'
+ root 'ideas#index'
+
+#  Setting up resourceful routes for ideas
+  resources :ideas do 
+    member do 
+      get :delete
+    end
+  end
+# setting resourceful routes for users
+  resources :users do 
+    member do 
+      get :delete
+    end
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
